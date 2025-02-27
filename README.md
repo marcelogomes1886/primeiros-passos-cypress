56caa25cddee8562c38b70f968246216c78fb8dd
marcelogomes1886 <marcelogomes1886@gmail.com>
2025-02-27 20:33:27 -0300

    User update scenario added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # My Cypress Automation
+
+**primeiros-passos-cypress** Repositório introdutório ao uso do Cypress, com testes automatizados para cenários de login (sucesso e falha). Inclui configuração do ambiente, exemplos práticos de autenticação e boas práticas em automação. Ideal para quem deseja iniciar na automação de testes com Cypress.
--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -8,16 +8,29 @@ describe('Orange HMH Tests', () => {
     loginButton: "[type='submit']",
     sectionTitleTopbar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastNameField: "[placeholder='Last Name']",
+    genericField: ".oxd-input--active",
+    submitButton: "[type='submit']",
   }
 
-  it('Login - Successful', () => {
+  it.only('User Info Update - Successful', () => {
     cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorList.dashboardGrid)
+    cy.get(selectorList.myInfoButton).click()
+    cy.get(selectorList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorList.lastNameField).clear().type('LastNameTest')
+    cy.get(selectorList.genericField).eq(4).clear().type('EmployeeIdTest')
+    cy.get(selectorList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    
+    
   })
   it('Login - Fail', () => {
     cy.visit('/auth/login')
