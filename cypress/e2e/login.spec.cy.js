bde799e872820ed65d988527e1a19a233a22ccd3
Marcelo Da Silveira Gomes <marcelogomes1886@gmail.com>
2026-08-03 16:56:02 -0300

    Added selectors list for spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,18 +1,36 @@
 describe('Orange HRM Tests', () => {
-  it.skip('Login - Success', () => {
+  
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: ".oxd-button",
+    dashboardTitle: ".oxd-topbar-header-breadcrumb > .oxd-text",
+    errorMessage: ".oxd-alert-content > .oxd-text"
+  }
+  
+  it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(cy.get('[name="username"]').type('Admin'))
-    cy.get(cy.get('[name="password"]').type('admin123'))
-    cy.get(cy.get('.oxd-button').click())
-    cy.location('pathname').should('equal' , '/web/index.php/dashboard/index')
-    cy.get(cy.get('.oxd-topbar-header-breadcrumb > .oxd-text')).contains('Dashboard')
+
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
+
+    cy.location('pathname')
+      .should('equal', '/web/index.php/dashboard/index')
+
+    cy.get(selectorsList.dashboardTitle)
+      .should('contain', 'Dashboard')
   })
+
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(cy.get('[name="username"]').type('Test'))
-    cy.get(cy.get('[name="password"]').type('Test'))
-    cy.get(cy.get('.oxd-button').click())
-    cy.get(cy.get('.oxd-alert-content > .oxd-text'))
-  }) 
+
+    cy.get(selectorsList.usernameField).type('Test')
+    cy.get(selectorsList.passwordField).type('Test')
+    cy.get(selectorsList.loginButton).click()
+
+    cy.get(selectorsList.errorMessage)
+      .should('contain', 'Invalid credentials')
+  })
 
 })
