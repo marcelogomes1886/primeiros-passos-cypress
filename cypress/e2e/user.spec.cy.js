717ea8eb348651701aa11679a2aa6b99e8bac474
Marcelo Da Silveira Gomes <marcelogomes1886@gmail.com>
2026-08-16 15:51:10 -0300

    Add random values on user spec with chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,60 +1,32 @@
-import userData from '../fixtures/user-data.json'
+import userData from '..//fixtures//user-data.json'
+import LoginPage from '..//pages//login.Page'
+import DashboardPage from '../pages/dashboardPage'
+import MenuPage from '../pages/menuPage'
+import MyInfoPage from '../pages/myInfoPage'
+
+const Chance = require('chance')
+
+const chance = new Chance()
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
+const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM Tests', () => {
-  
-  const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: ".oxd-button",
-    dashboardTitle: ".oxd-topbar-header-breadcrumb > .oxd-text",
-    errorMessage: ".oxd-alert-content > .oxd-text",
-    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
-    firstNameField: "[name='firstName']",
-    lastNameField: "[name='lastName']",
-    genericField: ".oxd-input--active",
-    
-  }
-  
 
-  
-  it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
+  it('User Info Update - Success', () => {
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
 
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
+    dashboardPage.checkDashboardPage()
 
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    menuPage.accessMyInfo()
 
-    cy.get(selectorsList.dashboardTitle).should('contain', 'Dashboard')
-
-    cy.get(selectorsList.myInfoButton).click()  
-
-    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
-
-    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-
-    cy.get(selectorsList.genericField).eq(3).clear().type('NicknameTest')
-
-    cy.get(selectorsList.genericField).eq(4).clear().type('Employee')
-
-    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
-
-    cy.get(selectorsList.genericField).eq(6).clear().type('DriversLicenseTest')
-
-   
-
+    myInfoPage.fillPersonalDetails(chance.first(), chance.last(), chance.string())
+    myInfoPage.fillEmployeeDetails('EmployId', 'otherId', 'Drivers Number', '2025-07-29', '123456', '0987654')
+    myInfoPage.fillStatus()
+    myInfoPage.saveForm()
   })
 
-  it('Login - Fail', () => {
-    cy.visit('/auth/login')
-
-    cy.get(selectorsList.usernameField).type(userData.userFail.username)
-    cy.get(selectorsList.passwordField).type(userData.userFail.password)
-    cy.get(selectorsList.loginButton).click()
-
-    cy.get(selectorsList.errorMessage)
-      .should('contain', 'Invalid credentials')
-  })
 
 })
